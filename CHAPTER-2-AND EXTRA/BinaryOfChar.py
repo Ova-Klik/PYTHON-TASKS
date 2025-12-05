f6fa1ca99a1a621d7f7252bebf29249e635c25be
I-am-Byte <victor.olaomo@outlook.com>
2025-12-05 05:57:39 +0100

    update

---
+++ CHAPTER-2-AND EXTRA/BinaryOfChar.py
@@ -0,0 +1,18 @@
+trigger=input("\nTo Display the Binary equivalent of (B,C,D,b,c,d,0,1,2,&,*,+): Press Yes\n\n").lower()
+
+if trigger =="yes":
+
+	print (f"The Binary Character of B is: {ord('B')}")
+	print (f"The Binary Character of C is: {ord('C')}")
+	print (f"The Binary Character of D is: {ord('D')}")
+	print (f"The Binary Character of b is: {ord('b')}")
+	print (f"The Binary Character of c is: {ord('c')}")
+	print (f"The Binary Character of d is: {ord('d')}")
+	print (f"The Binary Character of 0 is: {ord('0')}")
+	print (f"The Binary Character of 1 is: {ord('1')}")
+	print (f"The Binary Character of 2 is: {ord('2')}")
+	print (f"The Binary Character of & is: {ord('&')}")
+	print (f"The Binary Character of * is: {ord('*')}")
+	print (f"The Binary Character of * is: {ord('+')}\n")
+else:
+	print ("Enjoy Your Day.....")
---
+++ CHAPTER-2-AND EXTRA/SquareCubeTable.py
@@ -0,0 +1,15 @@
+print(f"PLEASE ENTER NUMBERS FROM 1 to 5\n")
+
+number_one=float(input("Please enter number: "))
+number_two=float(input("Please enter number: "))
+number_three=float(input("Please enter number: "))
+number_four=float(input("Please enter number: "))
+number_five=float(input("Please enter number: "))
+
+print(f"Number\t" , "Square\t", "cube\t\n" )
+print(f"{number_one}\t {number_one**2:.1f}\t {number_one**3:.1f}")
+print(f"{number_two}\t {number_two**2:.1f}\t {number_two**3:.1f}")
+print(f"{number_three}\t {number_three**2:.1f}\t {number_three**3:.1f}")
+print(f"{number_four}\t {number_four**2:.1f}\t {number_four**3:.1f}")
+print(f"{number_five}\t {number_five**2:.1f}\t {number_five**3:.1f}")
+
