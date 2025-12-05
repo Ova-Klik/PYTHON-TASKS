5e9e6104fe359a77be268c1fe46c223d38035a08
I-am-Byte <victor.olaomo@outlook.com>
2025-12-05 06:34:06 +0100

    update

---
+++ CHAPTER-2-AND EXTRA/DigitSeperator.py
@@ -0,0 +1,15 @@
+
+
+number=int(input("Please enter a single number 5 digits e.g (42339): "))
+
+number_one= number//10000
+number_two = (number//1000)%10
+number_three = (number//100)%10
+number_four = (number//10)%10
+number_five =number%10
+
+
+print(f"{number_one}   {number_two}   {number_three}   {number_four}   {number_five}")
+
+
+
