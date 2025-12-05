95fca9bb3c740b9c74659bce7080253443236793
I-am-Byte <victor.olaomo@outlook.com>
2025-12-05 03:35:44 +0100

    update

--- DECEMBER 04 EXERCISE/RunWayLength.py
+++ DECEMBER 04 EXERCISE/RunWayLength.py
@@ -1,7 +1,7 @@
 
 speed = float (input("Kindly enter speed: "))
 acceleration = float(input("PLease enter acceleration: "))
-length = (speed**2)/2*acceleration
+length = speed**2/(2*acceleration)
 
 
-print(f"The minimun runway for this airplane:{length}")
+print(f"The minimun runway for this airplane is: {length:.3f}meters") 
