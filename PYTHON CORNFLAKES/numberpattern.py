c1c6d3592099351e2d34c1342806f1bdbbefd18e
I-am-Byte <victor.olaomo@outlook.com>
2025-12-08 05:30:16 +0100

    new

--- PYTHON CORNFLAKES/numberpattern.py
+++ PYTHON CORNFLAKES/numberpattern.py
@@ -1,9 +1,8 @@
-# Number of rows
+
 number = 5
 
-# Generate the pattern
+
 for row in range(number, 0, -1):
     for column in range(row, 0, -1):
         print(column, end=' ')
-    print()  # Move to next line
-
+    print()  
