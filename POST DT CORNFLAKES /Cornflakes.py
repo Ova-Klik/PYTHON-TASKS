4e949395b998eee844153ad3144cc0ccc1b700a7
I-am-Byte <victor.olaomo@outlook.com>
2026-02-19 05:35:54 +0100

    post dt

--- POST DT CORNFLAKES /Cornflakes.py
+++ POST DT CORNFLAKES /Cornflakes.py
@@ -1,31 +1,25 @@
 
-#Write a program to print the first 15 multiples of 3 in a straight line. 
-#Write a program that print even numbers between 1-100
-
-
-
 
 
 sum=0;
 
 
-for number in range(20000):
-    
-    if number%10==0: 
+for number in range(0, 20001,10):
+  
         sum+=number
     
 print (sum)   
 
 
-for number in range(15):
+for number in range(1,16):
     
     if number%3==0:  
         print(number)
         
-for number in range(100):
+for number in range(1,101):
     
     if number%2==0:  
-        print (number)
+        print (number, end=" ")
         
         
 studentfail=0;
@@ -36,10 +30,12 @@ for number in range(0,15):
     score=int(input("Kindly enter score: "))
     if score<45:
         studentfail+=1;
+        print (f"FAILED")
     
     else:
     
         studentpass+=1;
+        print (f"PASSED")
         
 print (f"{studentpass} passed")
 print (f"{studentfail} failed")
@@ -58,7 +54,7 @@ for a in range(10):
 number=int(input("Kindly enter a number to get star: "))
 
 
-for a in range(number):
+for a in range(1, number+1):
     
     result= '*'* a
     print(f"{result}")
@@ -68,15 +64,27 @@ for a in range(number):
 number=int(input("Kindly enter a number to get a triangle factorial: "))
 
 
-for a in range(5,0, -1):
+for digits in range(number, 0, -1):
     
-    for b in range(a,0,-1):
+    for digit in range(digits, 0, -1):
     
-        print(b,end=" ")
-    
+        print(digit, end=" ")
+        
     print()
     
     
+number=int(input("Kindly enter a number to get a triangle factorial: "))
+
+
+for digits in range(number, 0, -1):
+    
+    for digit in range(digits, 0, -1):
+    
+        print(f"*",end=" ")
+ 
+        
+    print()
+
 
 
 
--- POST DT CORNFLAKES /CornflakesFunction.py
+++ POST DT CORNFLAKES /CornflakesFunction.py
@@ -4,29 +4,27 @@
 def mutiple_of_10(number):
     sum=0;
 
-    for number in range(20000):
+    for number in range(0, 20001,10):
         
-        if number%10==0: 
             sum+=number
         
     print (sum)    
 
 def mutiple_of_3(number):
 
-    for number in range(15):
+    for number in range(1,16):
     
     if number%3==0:  
         print(number)
             
 def even_number(number):
             
-    for number in range(100):
+    for number in range(1,101):
     
         if number%2==0:  
             print (number)
         
             
-  
    
 def student_score():
           
@@ -51,7 +49,6 @@ def mutiplication_table(number):
 
     number=int(input("Kindly enter a number to get the multiplication table: "))
 
-
     for a in range(10):
 
         result= number*a
@@ -63,7 +60,6 @@ def star(number):
 
     number=int(input("Kindly enter a number to get star: "))
 
-
     for a in range(number):
         
         result= '*'* a
@@ -74,7 +70,7 @@ def number_triangle(number):
     number=int(input("Kindly enter a number to get a triangle factorial: "))
 
 
-    for a in range(5,0, -1):
+    for a in range(number,0, -1):
         
         for b in range(a,0,-1):
         
@@ -82,5 +78,6 @@ def number_triangle(number):
         
         print()
         
+
 
     
